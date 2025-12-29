2dcc0eb2bea78bee7100d3b024966a7138ebbe6c
joantrave <joan.t@iatiseguros.com>
2025-12-29 21:19:34 +0100

    Put define macros to header files

--- src/ui/print_layout.h
+++ src/ui/print_layout.h
@@ -1,3 +1,8 @@
+#ifndef TETONOR_PRINT_LAYOUT_H
+#define TETONOR_PRINT_LAYOUT_H
+
 #include "../game/game_port.h"
 
 void print_layout(GamePort* game_port);
+
+#endif
--- src/ui/ui.h
+++ src/ui/ui.h
@@ -1,3 +1,8 @@
+#ifndef TETONOR_UI_H
+#define TETONOR_UI_H
+
 #include "../game/game_port.h"
 
 void ui_run(GamePort* game_port);
+
+#endif
