97cadf0a1dda0b83d766ccdb1fb7680c5b27cee1
joantrave <joan.t@iatiseguros.com>
2026-01-09 14:32:01 +0100

    New layout

--- src/ui/print_layout.c
+++ src/ui/print_layout.c
@@ -20,8 +20,9 @@ void _print_sum_matrix(NumberGeneration* number_generation)
     {
         for (int j = 0; j < 4; j++)
         {
-            mvprintw(1 + i, 5 * j, "%d", number_generation->sum_matrix[i][j]);
+            mvprintw(2 + 2 * i, 5 * j, "|%d", number_generation->sum_matrix[i][j]);
         }
+        mvprintw(2 + 2 * i + 1, 0, "--------------------");
     }
 }
 
@@ -29,6 +30,11 @@ void _print_available_array(NumberGeneration* number_generation)
 {
     for (int i = 0; i < 16; i++)
     {
-        mvprintw(7, 3 * i, "%d", number_generation->available_array[i]);
+        if (number_generation->available_array[i] == -1)
+        {
+            mvprintw(12, 4 * i, "|");
+            continue;
+        }
+        mvprintw(12, 4 * i, "|%d", number_generation->available_array[i]);
     }
 }
