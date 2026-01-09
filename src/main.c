ca29ea9671093ea490e0b25cf0f2c721cb1a6e66
joantrave <joan.t@iatiseguros.com>
2026-01-09 13:33:20 +0100

    Move seed to main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
 #include <ncurses.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "ui/ui.h"
 
 int main(void)
 {
+    srand((unsigned)time(NULL));
     GamePort* game_port = create_game_port();
     ui_run(game_port);
     destroy_game_port(game_port);
