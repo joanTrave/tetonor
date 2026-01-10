2ed9d16d9629f660b9ffcd127c93579e6aa88a66
joantrave <jtravegordillo@gmail.com>
2026-01-10 23:30:24 +0100

    Add main menu first design

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -2,11 +2,20 @@
 
 #include "ui.h"
 #include "init_screen.h"
+#include "menu.h"
 #include "print_layout.h"
 
 void ui_run(GamePort* game_port)
 {
     init_screen();
+    MenuResult menu_result = ui_show_start_menu();
+    if (menu_result == MENU_EXIT)
+    {
+        endwin();
+        return;
+    }
+    clear();
+    refresh();
     print_layout(game_port);
     int ch = getch();
     while (ch != 27)
